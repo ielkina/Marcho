05b6564098d1d88dc15c01536f82475f5ccfb935
Iryna <ielkinairinka@gmail.com>
2023-01-12 22:24:08 +0300

    addet adapt promo

--- src/js/main.js
+++ src/js/main.js
@@ -72,7 +72,7 @@ $(function () {
     },
   });
   $(".slider").slick({
-    dots: true, //кнопки 
+    dots: true, //кнопки
     arrows: false, //убирает стрелки
     // fade: true,//плавное появление, отключает перелистывание.
     // infinite: true,
@@ -82,32 +82,42 @@ $(function () {
     // speed: 30000
     // speed: 3000
   });
-  $(".product-item__star, .product-one__star, .comments__content-star").rateYo({
+  // $(".product-item__star, .product-one__star, .comments__content-star").rateYo({
+  //   starWidth: "17px",
+  //   normalFill: "#ccccce",
+  //   ratedFill: "#ffc35b",
+  //   // ratedStroke: "#ffc35b",
+  //   // rating: 4,
+  //   // spacing: "5px",
+  //   // multiColor: {
+  //   //   "startColor": "#FF0000", //RED
+  //   //   "endColor": "#00FF00"  //GREEN
+  //   // },
+  //   // numStars: 10,
+  //   // maxValue: 1,
+  //   // numStars: 1,
+  //   // starWidth: "40px",
+  //   // precision: 2,
+  //   // rating: "50%",
+  //   // precision: 0,
+  //   // rating: 1.5,
+  //   // halfStar: true,
+  //   // fullStar: true,
+  //   // rating: 3.2,
+  //   readOnly: true,
+  //   starSvg:
+  //     ' <svg class="icon">' +
+  //     ' <use xlink:href="img/icons/sprite.svg#icon-star"></use>' +
+  //     "</svg>",
+  // });
+  $(".product-item__star").rateYo({
     starWidth: "17px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
-    // ratedStroke: "#ffc35b",
-    // rating: 4,
-    // spacing: "5px",
-    // multiColor: {
-    //   "startColor": "#FF0000", //RED
-    //   "endColor": "#00FF00"  //GREEN
-    // },
-    // numStars: 10,
-    // maxValue: 1,
-    // numStars: 1,
-    // starWidth: "40px",
-    // precision: 2,
-    // rating: "50%",
-    // precision: 0,
-    // rating: 1.5,
-    // halfStar: true,
-    // fullStar: true,
-    // rating: 3.2,
     readOnly: true,
     starSvg:
-      ' <svg class="icon">' +
-      ' <use xlink:href="img/icons/sprite.svg#icon-star"></use>' +
+      '<svg class="product-item__star-icon">' +
+      '<use xlink:href="img/icons/sprite.svg#icon-star"></use>' +
       "</svg>",
   });
 
